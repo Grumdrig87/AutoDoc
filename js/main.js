74be5571783bf0959f861202d66588894dc344db
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2022-04-13 13:02:42 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  
+  // burger
+  $('[data-burger]').click(function() {
+      $('html').toggleClass("open");
+      $(this).toggleClass("open");
+      $('[data-nav]').toggleClass("open");
+      $('body').toggleClass('open');
+  });
+  
+})
