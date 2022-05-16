e6814be7f204bd4439a387103d2c62f9618fd4ad
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2022-05-16 18:01:28 +0300

    16.05.22 18-01

--- js/main.js
+++ js/main.js
@@ -16,7 +16,101 @@ jQuery(document).ready(function($) {
       }
     });
   };
+
+  // upload
+
   
+  if (jQuery('[data-dropzone]').length > 0) {
+    var dropZone = $('[data-dropzone]'),
+          maxFileSize = 10000000; // максимальный размер фалйа - 10 мб.
+      
+      // Проверка поддержки браузером
+      if (typeof(window.FileReader) == 'undefined') {
+          dropZone.text('Не поддерживается браузером!');
+          dropZone.addClass('error');
+      }
+      
+      // Добавляем класс hover при наведении
+      dropZone[0].ondragover = function() {
+          dropZone.addClass('hover');
+          return false;
+      };
+      
+      // Убираем класс hover
+      dropZone[0].ondragleave = function() {
+          dropZone.removeClass('hover');
+          return false;
+      };
+      
+      // Обрабатываем событие Drop
+      dropZone[0].ondrop = function(event) {
+          event.preventDefault();
+          dropZone.removeClass('hover');
+          dropZone.addClass('drop');
+          
+          var file = event.dataTransfer.files[0];
+          
+          // Проверяем размер файла
+          if (file.size > maxFileSize) {
+              dropZone.find('.addnew__drop').text('Файл слишком большой!');
+              dropZone.addClass('error');
+              return false;
+          }
+          
+          // Создаем запрос
+          var xhr = new XMLHttpRequest();
+          xhr.upload.addEventListener('progress', uploadProgress, false);
+          xhr.onreadystatechange = stateChange;
+          xhr.open('POST', '/upload.php');
+          xhr.setRequestHeader('X-FILE-NAME', file.name);
+          xhr.send(file);
+      };
+      
+      // Показываем процент загрузки
+      function uploadProgress(event) {
+          var percent = parseInt(event.loaded / event.total * 100);
+          dropZone.find('.addnew__drop').text('Загрузка: ' + percent + '%');
+      }
+      
+      // Пост обрабочик
+      function stateChange(event) {
+          if (event.target.readyState == 4) {
+              if (event.target.status == 200) {
+                  dropZone.find('.addnew__drop').text('Загрузка успешно завершена!');
+              } else {
+                  dropZone.find('.addnew__drop').text('Произошла ошибка!');
+                  dropZone.addClass('error');
+              }
+          }
+      }
+    }
+    if ($('[data-file]').length > 0) {
+      $('[data-file]').on('change', function() {
+        // var fileBuffer=[];
+        // Array.prototype.push.apply( fileBuffer, $('[data-file]').get(0).files );
+        var name_file = []; 
+        
+          for(var i = 0; i < $(this).get(0).files.length; ++i) {
+            var reader = new FileReader();
+            $("[data-dropfoto]").append('<div><img src="" alt="" class="image" data-index="'+i+'"></div>');
+            
+            reader.onload = function (e) {
+              name_file.push(e.target.result);
+            };
+            reader.readAsDataURL($(this).get(0).files[i])
+            $('[data-index="'+i+'"]').attr('src', $(this).get(0).files[i])
+          } 
+          console.log(name_file);
+          // $("[data-dropfoto]").html(name_file); 
+          $('span.del').click(function(){
+            $(this).parent().hide();
+            var spanId = $(this).parent().index();
+            for(var i = 0; i < fileBuffer.length; ++i) {
+              delete fileBuffer[spanId];
+            } 
+          })
+      });
+    }
   // burger
   $('[data-burger]').click(function() {
       $('html').toggleClass("open");
