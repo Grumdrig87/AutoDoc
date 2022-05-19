6e0d882963f9aec69913a94429e5e33d9dff4aeb
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2022-05-19 16:18:28 +0300

    19.05.22 16-18

--- js/upload.js
+++ js/upload.js
@@ -91,6 +91,27 @@ jQuery(document).ready(function($) {
     });
   }
 
+// ________________________________________________
+
+$('#multiupload').fileapi({
+    url: 'http://rubaxa.org/FileAPI/server/ctrl.php',
+    multiple: true,
+    elements: {
+       empty: { show: '.b-upload__hint' },
+       list: '.js-files',
+       file: {
+          tpl: '.js-file-tpl',
+          preview: {
+             el: '.b-thumb__preview',
+             width: 150,
+             height: 120
+          },
+       }
+    }
+ });
+ $('.addFile input').on('change', function () {
+    $('.addFile-icon').appendTo('.b-upload__files');
+ })
 
   
 })
